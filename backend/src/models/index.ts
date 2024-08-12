84cc5b7178229061161244be20b71fce1d676a63
gusalseh <maroonever96@gmail.com>
2024-08-13 00:37:16 +0900

    initial backend setting

---
+++ backend/src/models/index.ts
@@ -0,0 +1,50 @@
+import fs from "fs";
+import path from "path";
+import { Sequelize, DataTypes } from "sequelize";
+import process from "process";
+
+const basename = path.basename(__filename);
+const env = process.env.NODE_ENV || "development";
+const config = require(__dirname + "/../config/config.json")[env];
+const db: any = {};
+
+let sequelize: Sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(
+    process.env[config.use_env_variable] as string,
+    config
+  );
+} else {
+  sequelize = new Sequelize(
+    config.database as string,
+    config.username as string,
+    config.password as string,
+    config
+  );
+}
+
+fs.readdirSync(__dirname)
+  .filter((file) => {
+    return (
+      file.indexOf(".") !== 0 &&
+      file !== basename &&
+      file.slice(-3) === ".ts" &&
+      file.indexOf(".test.ts") === -1
+    );
+  })
+  .forEach((file) => {
+    const model = require(path.join(__dirname, file))(sequelize, DataTypes);
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach((modelName) => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+export { sequelize };
+export default db;
