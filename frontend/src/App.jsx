655dbc82bdd72b763a8648a72b27cba0bc55b3f7
kang98kang <skystar0071@naver.com>
2024-08-14 18:40:10 +0900

    update ant, AuthContext

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Login from './routes/Login';
+import { AuthProvider } from './contexts/AuthContext';
 
 function App() {
   return (
     <div>
-      <Login />
+      <AuthProvider>
+        <Login />
+      </AuthProvider>
     </div>
   );
 }
