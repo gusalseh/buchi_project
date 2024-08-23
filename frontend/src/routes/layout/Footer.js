c03ee34ff33ff89a3f3ccc036a7d58f81cfe95fe
kang98kang <skystar0071@naver.com>
2024-08-23 18:27:01 +0900

    add frontend Footer

---
+++ frontend/src/routes/layout/Footer.js
@@ -0,0 +1,18 @@
+export default function Footer() {
+  return (
+    <footer
+      style={{
+        width: 1920,
+        height: 270,
+        padding: '60px 340px 120px 340px',
+        alignItems: 'flex-start',
+        gap: 80,
+        alignSelf: 'stretch',
+        backgroundColor: '#FDF5F4',
+        bottom: 0,
+      }}
+    >
+      Copyright @ 2024 All right reserved by Buchi
+    </footer>
+  );
+}
