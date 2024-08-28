396bfd9cf4b5632e30d9d68cd1f0d5087b97f41a
gusalseh <maroonever96@gmail.com>
2024-08-29 03:06:41 +0900

    update filter component

--- frontend/src/components/common/Filter.js
+++ frontend/src/components/common/Filter.js
@@ -1,15 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { DownOutlined } from '@ant-design/icons';
-import { Modal } from 'antd';
+import { Modal, Select } from 'antd';
 import { Layout, Typography, DatePicker, TimePicker, InputNumber, Row, Col, Button } from 'antd';
-import { CalendarOutlined, ClockCircleOutlined, UserOutlined } from '@ant-design/icons';
+import { CalendarOutlined, ClockCircleOutlined, UserOutlined, SearchOutlined } from '@ant-design/icons';
 import UserLocation from './UserLocationModal';
 import { fetchSelectedLocation } from '../../features/userLocation';
 import LoginAlert from '../alert/LoginAlert';
 
 const { Content } = Layout;
 const { Title, Text } = Typography;
+const { Option } = Select;
 
 const Filter = () => {
   const user = useSelector((state) => state.user.user);
@@ -17,6 +18,8 @@ const Filter = () => {
   const [isModalVisible, setIsModalVisible] = useState(false);
   const [modalKey, setModalKey] = useState(0); // 모달을 다시 렌더링하기 위한 key
   const [isLoginAlertVisible, setIsLoginAlertVisible] = useState(false);
+  const [selectedDate, setSelectedDate] = useState(null); // TODO: 날짜, 시간, 인원의 정보를 한꺼번에 state 관리해주는게 좋아보임
+  const [selectedTime, setSelectedTime] = useState(null);
 
   useEffect(() => {
     const fetchLocation = async () => {
@@ -52,6 +55,30 @@ const Filter = () => {
 
   const handleLoginAlertClose = () => {
     setIsLoginAlertVisible(false); // 로그인 경고 모달 닫기
+  };
+
+  const getTodayFormatted = () => {
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = today.getMonth() + 1;
+    const day = today.getDate();
+
+    return `${year}년 ${month < 10 ? `0${month}` : month}월 ${day < 10 ? `0${day}` : day}일`;
+  };
+
+  const disabledDate = (current) => {
+    const today = new Date();
+    today.setHours(0, 0, 0, 0);
+
+    return current && current.toDate() < today;
+  };
+
+  const handleDateChange = (dateString) => {
+    setSelectedDate(dateString);
+  };
+
+  const handleChange = (value) => {
+    setSelectedTime(value);
   };
 
   return (
@@ -162,23 +189,69 @@ const Filter = () => {
         <Row style={{ gap: 22, justifyContent: 'center', width: '1920px' }}>
           <Col style={{ MinWidth: 240 }}>
             <DatePicker
-              placeholder="회식 날짜 입력"
-              style={{ MinWidth: 240, width: 240 }}
+              placeholder={getTodayFormatted()}
+              value={selectedDate}
+              style={{ MinWidth: 240, width: 240, height: 40 }}
+              disabledDate={disabledDate}
+              format="YYYY년 MM월 DD일"
               suffixIcon={<CalendarOutlined />}
+              onChange={handleDateChange}
+              allowClear={false}
             />
           </Col>
           <Col style={{ MinWidth: 240 }}>
-            <TimePicker
-              placeholder="회식 시간대 입력"
-              style={{ MinWidth: 240, width: 240 }}
-              suffixIcon={<ClockCircleOutlined />}
-            />
+            <Select
+              placeholder="저녁회식"
+              style={{ width: 240, height: 40, color: 'rgba(0, 0, 0, 0.25)' }}
+              suffixIcon={<ClockCircleOutlined style={{ fontSize: 15 }} />}
+              onChange={handleChange}
+              value={selectedTime}
+            >
+              <Option value="점심회식" style={{ margin: '0 0 10px 0' }}>
+                점심회식
+              </Option>
+              <Option value="저녁회식">저녁회식</Option>
+            </Select>
           </Col>
           <Col style={{ MinWidth: 240 }}>
             <InputNumber
-              placeholder="인원수 입력"
-              style={{ MinWidth: 240, width: 240 }}
+              placeholder="3명"
+              style={{
+                MinWidth: 240,
+                width: 240,
+                height: 40,
+                textAlign: 'center',
+                display: 'flex',
+                alignItems: 'center',
+                color: 'rgba(0, 0, 0, 0.25)',
+              }}
               suffixIcon={<UserOutlined />}
+              min={1}
+              max={100}
+              onChange={(value) => {
+                if (value < 1) {
+                  return 1;
+                }
+                if (value > 100) {
+                  return 100;
+                }
+                return value;
+              }}
+            />
+          </Col>
+          <Col style={{ MinWidth: 40 }}>
+            <Button
+              icon={<SearchOutlined style={{ fontSize: '20px', color: 'rgba(0, 0, 0, 0.65)' }} />}
+              style={{
+                width: 40,
+                height: 40,
+                borderRadius: '6px',
+                border: '1px solid #d9d9d9',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                padding: 0,
+              }}
             />
           </Col>
         </Row>
