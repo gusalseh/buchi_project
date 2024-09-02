234f1c46e859deeaef05629595c3138351184eba
gusalseh <maroonever96@gmail.com>
2024-09-02 16:06:29 +0900

    change config info from ec2 mysql to rds

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -2,12 +2,17 @@ require('dotenv').config();
 const env = process.env;
 
 const development = {
-  username: env.MYSQL_USERNAME,
-  password: env.MYSQL_PASSWORD,
-  database: env.MYSQL_DATABASE,
-  host: env.MYSQL_HOST,
+  // username: env.MYSQL_USERNAME,
+  // password: env.MYSQL_PASSWORD,
+  // database: env.MYSQL_DATABASE,
+  // host: env.MYSQL_HOST,
+  // dialect: 'mysql',
+  // port: 3306,
+  username: env.RDS_USERNAME,
+  password: env.RDS_PASSWORD,
+  database: env.RDS_DATABASE,
+  host: env.RDS_HOST,
   dialect: 'mysql',
-  port: 3306,
 };
 
 const test = {
