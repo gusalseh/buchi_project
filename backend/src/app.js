2f3b3c39b9f4aa516c91709be11636b5d0276d65
gusalseh <maroonever96@gmail.com>
2024-09-02 16:00:14 +0900

    change server port 3000 to 80

--- backend/src/app.js
+++ backend/src/app.js
@@ -81,7 +81,7 @@ app.post('/geocode', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 80;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   sequelize.sync();
