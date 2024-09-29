ee3dddd4607607850596952b7e90e119bc2315e6
kang98kang <skystar0071@naver.com>
2024-09-29 17:33:13 +0900

    add promotion imgs

--- frontend/src/components/common/Promotion.js
+++ frontend/src/components/common/Promotion.js
@@ -1,13 +1,15 @@
 import { Carousel } from 'antd';
-import promotion1 from '../../assets/Img/promotiontest1.webp';
-import promotion2 from '../../assets/Img/promotiontest2.webp';
-import promotion3 from '../../assets/Img/promotiontest3.webp';
+import promotion1 from '../../assets/Img/promotion/promotion1.png';
+import promotion2 from '../../assets/Img/promotion/promotion2.png';
+import promotion3 from '../../assets/Img/promotion/promotion3.png';
+import promotion4 from '../../assets/Img/promotion/promotion4.png';
+import promotion5 from '../../assets/Img/promotion/promotion5.png';
 
-const images = [promotion1, promotion2, promotion3];
+const images = [promotion1, promotion2, promotion3, promotion4, promotion5];
 
 const Promotion = () => {
   return (
-    <div style={{ width: '100%', height: 360, textAlign: 'center', backgroundColor: 'gray' }}>
+    <div style={{ width: 1920, height: 360, textAlign: 'center', backgroundColor: 'gray' }}>
       <Carousel autoplay dotPosition="top">
         {images.map((image, index) => (
           <div key={index}>
